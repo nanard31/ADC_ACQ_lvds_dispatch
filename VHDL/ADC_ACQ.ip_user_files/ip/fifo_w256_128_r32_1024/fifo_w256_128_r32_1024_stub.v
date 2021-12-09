07aa0fe4f8c5bcb0f8add4c8eb0dde38fc1ead98
bbertrand-eng <bernard.bertrand@irap.omp.eu>
2021-12-09 14:37:25 +0100

    Modife dans le ramtest.v, Architecture precedent qui fonctionne en simulation  et compile  maintenant.

--- VHDL/ADC_ACQ.ip_user_files/ip/fifo_w256_128_r32_1024/fifo_w256_128_r32_1024_stub.v
+++ VHDL/ADC_ACQ.ip_user_files/ip/fifo_w256_128_r32_1024/fifo_w256_128_r32_1024_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
-// Date        : Wed Oct 13 15:41:22 2021
-// Host        : DESKTOP-BNTCRVM running 64-bit major release  (build 9200)
+// Date        : Thu Dec  9 10:45:59 2021
+// Host        : DESKTOP-BSP8Q2B running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
-//               c:/Projets/DORN/ADC_ACQ/VHDL/Sources/Core/fifo_w256_128_r32_1024/fifo_w256_128_r32_1024_stub.v
+//               c:/SVN_GIT/ADC_ACQ_lvds_dispatch/VHDL/Sources/Core/fifo_w256_128_r32_1024/fifo_w256_128_r32_1024_stub.v
 // Design      : fifo_w256_128_r32_1024
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7a75tfgg484-1
--- VHDL/ADC_ACQ.ip_user_files/ip/fifo_w32_1024_r256_128/fifo_w32_1024_r256_128_stub.vhdl
+++ VHDL/ADC_ACQ.ip_user_files/ip/fifo_w32_1024_r256_128/fifo_w32_1024_r256_128_stub.vhdl
@@ -1,10 +1,10 @@
 -- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
--- Date        : Wed Oct 13 15:54:08 2021
--- Host        : DESKTOP-BNTCRVM running 64-bit major release  (build 9200)
+-- Date        : Thu Dec  9 10:46:00 2021
+-- Host        : DESKTOP-BSP8Q2B running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
---               c:/Projets/DORN/ADC_ACQ/VHDL/Sources/Core/fifo_w32_1024_r256_128/fifo_w32_1024_r256_128_stub.vhdl
+--               c:/SVN_GIT/ADC_ACQ_lvds_dispatch/VHDL/Sources/Core/fifo_w32_1024_r256_128/fifo_w32_1024_r256_128_stub.vhdl
 -- Design      : fifo_w32_1024_r256_128
 -- Purpose     : Stub declaration of top-level module interface
 -- Device      : xc7a75tfgg484-1
